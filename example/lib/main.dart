7db6ac8615c1a963b065cf83793e90e066607e3e
fangkyi02 <lvch@ztesa.com.cn>
2022-08-13 13:47:50 +0800

    统一加上FL前缀

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,69 +1,52 @@
-import 'package:example/pages/v1/index.dart';
-import 'package:example/pages/v2/index.dart';
-import 'package:example/pages/v3/index.dart';
-import 'package:example/pages/v4/index.dart';
 import 'package:flutter/material.dart';
-import 'package:rlstyles/Component/CssRule.dart';
 import 'package:rlstyles/main.dart';
 
 void main() {
-  runApp(App());
+  runApp(Home());
 }
 
-class App extends HookWidget {
-  const App({Key? key}) : super(key: key);
-
-  Widget renderView() {
-    return View(
-      styles: {
-        CssRule.backgroundColor: 'red',
-        CssRule.width: '100%',
-        CssRule.height: '100%',
-        CssRule.justifyContent: 'center',
-        CssRule.alignItems: 'center',
-      },
-      children: [
-        View(
-          styles: {
-            CssRule.alignItems: 'center',
-            CssRule.backgroundColor: 'blue'
-          },
-          children: [
-            TextView(
-              '1232',
-              styles: {CssRule.fontSize: 26},
-            ),
-            TextView(
-              '1',
-              styles: {
-                CssRule.fontSize: 50,
-              },
-            )
-          ],
-        )
-      ],
-    );
-  }
-
+class Home extends HookWidget {
+  const Home({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    Widget renderBody() {
+      return View(
+        children: [
+          View(
+            onClick: () {
+              print('点击');
+            },
+            styles: mergeStyle([FL_Color(color: Colors.blue)]),
+            children: [TextView('中国')],
+          )
+        ],
+        styles: mergeStyle([
+          FL_FlexColumn,
+          FL_AlignCenter,
+          FL_JustifyCenter,
+          FL_Width(size: 100),
+          FL_BgColor(color: Colors.red),
+          FL_Height(size: 200),
+        ]),
+      );
+    }
+
     return ScreenUtilInit(
-      designSize: const Size(360, 640),
-      builder: () => MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter_ScreenUtil',
-        initialRoute: '/v4',
-        routes: {
-          // 头部搜索编辑框
-          '/': (BuildContext context) => Scaffold(body: SafeArea(child: V1())),
-          '/v2': (BuildContext context) =>
-              Scaffold(body: SafeArea(child: V2())),
-          '/v3': (BuildContext context) =>
-              Scaffold(body: SafeArea(child: V3())),
-          '/v4': (BuildContext context) =>
-              Scaffold(body: SafeArea(child: V4())),
-        },
-      ),
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'First Method',
+          // You can use the library anywhere in the app even in theme
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            textTheme: Typography.englishLike2018.apply(fontSizeFactor: 1.sp),
+          ),
+          home: Scaffold(body: renderBody()),
+        );
+      },
     );
   }
 }
--- lib/Tailwind/index.dart
+++ lib/Tailwind/index.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: non_constant_identifier_names
+
 import 'package:rlstyles/Component/CssRule.dart';
 
 // 合并所有样式
@@ -10,161 +12,162 @@ var mergeStyle = (List styles) {
 };
 
 // row
-const FlexRow = {CssRule.flexDirection: 'row'};
-const FlexRowReverse = {CssRule.flexDirection: 'row-reverse'};
+const FL_FlexRow = {CssRule.flexDirection: 'row'};
+const FL_FlexRowReverse = {CssRule.flexDirection: 'row-reverse'};
 
 // column
-const FlexColumn = {CssRule.flexDirection: 'column'};
-const FlexColumnReverse = {CssRule.flexDirection: 'column-reverse'};
+const FL_FlexColumn = {CssRule.flexDirection: 'column'};
+const FL_FlexColumnReverse = {CssRule.flexDirection: 'column-reverse'};
 
 // wrap
-const FlexWrap = {CssRule.flexWrap: 'wrap'};
-const FlexNoWrap = {CssRule.flexWrap: 'nowrap'};
+const FL_FlexWrap = {CssRule.flexWrap: 'wrap'};
+const FL_FlexNoWrap = {CssRule.flexWrap: 'nowrap'};
 
 // Justify
-const JustifyStart = {CssRule.justifyContent: 'flex-start'};
-const JustifyCenter = {CssRule.justifyContent: 'center'};
-const JustifyEnd = {CssRule.justifyContent: 'flex-end'};
-const JustifyBetween = {CssRule.justifyContent: 'space-between'};
-const JustifyAround = {CssRule.justifyContent: 'space-around'};
+const FL_JustifyStart = {CssRule.justifyContent: 'flex-start'};
+const FL_JustifyCenter = {CssRule.justifyContent: 'center'};
+const FL_JustifyEnd = {CssRule.justifyContent: 'flex-end'};
+const FL_JustifyBetween = {CssRule.justifyContent: 'space-between'};
+const FL_JustifyAround = {CssRule.justifyContent: 'space-around'};
 
 // align
-const AlignStart = {CssRule.alignItems: 'flex-start'};
-const AlignCenter = {CssRule.alignItems: 'center'};
-const AlignEnd = {CssRule.alignItems: 'flex-end'};
+const FL_AlignStart = {CssRule.alignItems: 'flex-start'};
+const FL_AlignCenter = {CssRule.alignItems: 'center'};
+const FL_AlignEnd = {CssRule.alignItems: 'flex-end'};
 
 // padding
-final padding = ({dynamic size}) => {CssRule.padding: size};
-final paddingLeft = ({dynamic size}) => {CssRule.paddingLeft: size};
-final paddingRight = ({dynamic size}) => {CssRule.paddingRight: size};
-final paddingTop = ({dynamic size}) => {CssRule.paddingTop: size};
-final paddingBottom = ({dynamic size}) => {CssRule.paddingBottom: size};
+final FL_Padding = ({dynamic size}) => {CssRule.padding: size};
+final FL_PaddingLeft = ({dynamic size}) => {CssRule.paddingLeft: size};
+final FL_PaddingRight = ({dynamic size}) => {CssRule.paddingRight: size};
+final FL_PaddingTop = ({dynamic size}) => {CssRule.paddingTop: size};
+final FL_PaddingBottom = ({dynamic size}) => {CssRule.paddingBottom: size};
 
 // margin
-final margin = ({dynamic size}) => {CssRule.margin: size};
-final marginLeft = ({dynamic size}) => {CssRule.marginLeft: size};
-final marginRight = ({dynamic size}) => {CssRule.marginRight: size};
-final marginTop = ({dynamic size}) => {CssRule.marginTop: size};
-final marginBottom = ({dynamic size}) => {CssRule.marginBottom: size};
+final FL_Margin = ({dynamic size}) => {CssRule.margin: size};
+final FL_MarginLeft = ({dynamic size}) => {CssRule.marginLeft: size};
+final FL_MarginRight = ({dynamic size}) => {CssRule.marginRight: size};
+final FL_MarginTop = ({dynamic size}) => {CssRule.marginTop: size};
+final FL_MarginBottom = ({dynamic size}) => {CssRule.marginBottom: size};
 
 // width
-final width = ({dynamic size}) => {CssRule.width: size};
-final wFull = {CssRule.width: '100%'};
+final FL_Width = ({dynamic size}) => {CssRule.width: size};
+final FL_WFull = {CssRule.width: '100%'};
 
 // height
-final height = ({dynamic size}) => {CssRule.height: size};
-final hFull = {CssRule.height: '100%'};
+final FL_Height = ({dynamic size}) => {CssRule.height: size};
+final FL_HFull = {CssRule.height: '100%'};
 
 // fontSize
-final fSize = ({dynamic size}) => {CssRule.fontSize: size};
-final fSizeLg = {CssRule.fontSize: '20px'};
-final fSizeSm = {CssRule.fontSize: '15px'};
-final fSizeXs = {CssRule.fontSize: '12px'};
+final FL_FSize = ({dynamic size}) => {CssRule.fontSize: size};
+final FL_FSizeLg = {CssRule.fontSize: '20px'};
+final FL_SizeSm = {CssRule.fontSize: '15px'};
+final FL_FSizeXs = {CssRule.fontSize: '12px'};
 
 // fontColor
-final fColor = ({dynamic color}) => {CssRule.color: color};
-final fColorBlack = {CssRule.color: '#000'};
-final fColorWhite = {CssRule.color: '#fff'};
-final fColorRed = {CssRule.color: '#f00'};
-final fColorGreen = {CssRule.color: '#0f0'};
-final fColorBlue = {CssRule.color: '#00f'};
+final FL_Color = ({dynamic color}) => {CssRule.color: color};
+final FL_ColorBlack = {CssRule.color: '#000'};
+final FL_ColorWhite = {CssRule.color: '#fff'};
+final FL_ColorRed = {CssRule.color: '#f00'};
+final FL_ColorGreen = {CssRule.color: '#0f0'};
+final FL_ColorBlue = {CssRule.color: '#00f'};
 
 // radius
-final radius = ({dynamic size}) => {CssRule.borderRadius: size};
-final radiusLg = {CssRule.borderRadius: '5px'};
-final radiusSm = {CssRule.borderRadius: '2px'};
-final radiusXs = {CssRule.borderRadius: '1px'};
-final rLeftTop = ({dynamic size}) => {CssRule.borderTopLeftRadius: size};
-final rLeftBottom = ({dynamic size}) => {CssRule.borderBottomLeftRadius: size};
-final rRightTop = ({dynamic size}) => {CssRule.borderTopRightRadius: size};
-final rRightBottom =
+final FL_Radius = ({dynamic size}) => {CssRule.borderRadius: size};
+final FL_RadiusLg = {CssRule.borderRadius: '5px'};
+final FL_RadiusSm = {CssRule.borderRadius: '2px'};
+final FL_RadiusXs = {CssRule.borderRadius: '1px'};
+final FL_RLeftTop = ({dynamic size}) => {CssRule.borderTopLeftRadius: size};
+final FL_RLeftBottom =
+    ({dynamic size}) => {CssRule.borderBottomLeftRadius: size};
+final FL_RRightTop = ({dynamic size}) => {CssRule.borderTopRightRadius: size};
+final FL_RRightBottom =
     ({dynamic size}) => {CssRule.borderBottomRightRadius: size};
 
 // border
-final border = ({dynamic size, dynamic color}) => {
+final FL_Border = ({dynamic size, dynamic color}) => {
       CssRule.borderWidth: size,
       CssRule.borderStyle: 'solid',
       CssRule.borderColor: color
     };
-final bLeftTop = ({dynamic size, dynamic color}) => {
+final FL_BLeftTop = ({dynamic size, dynamic color}) => {
       CssRule.borderTopLeftRadius: size,
       CssRule.borderLeftWidth: size,
       CssRule.borderLeftStyle: 'solid',
       CssRule.borderLeftColor: color
     };
-final bLeftBottom = ({dynamic size, dynamic color}) => {
+final FL_BLeftBottom = ({dynamic size, dynamic color}) => {
       CssRule.borderBottomLeftRadius: size,
       CssRule.borderLeftWidth: size,
       CssRule.borderLeftStyle: 'solid',
       CssRule.borderLeftColor: color
     };
-final bRightTop = ({dynamic size, dynamic color}) => {
+final FL_BRightTop = ({dynamic size, dynamic color}) => {
       CssRule.borderTopRightRadius: size,
       CssRule.borderRightWidth: size,
       CssRule.borderRightStyle: 'solid',
       CssRule.borderRightColor: color
     };
-final bRightBottom = ({dynamic size, dynamic color}) => {
+final FL_BRightBottom = ({dynamic size, dynamic color}) => {
       CssRule.borderBottomRightRadius: size,
       CssRule.borderRightWidth: size,
       CssRule.borderRightStyle: 'solid',
       CssRule.borderRightColor: color
     };
 
-const borderSolid = {CssRule.borderStyle: 'solid'};
+const FL_BorderSolid = {CssRule.borderStyle: 'solid'};
 
 // backGroundColor
-final bgColor = ({dynamic color}) => {CssRule.backgroundColor: color};
+final FL_BgColor = ({dynamic color}) => {CssRule.backgroundColor: color};
 
 // position
-final absolute = {CssRule.position: 'abs'};
-final relative = {CssRule.position: 'rel'};
+final FL_Absolute = {CssRule.position: 'abs'};
+final FL_Relative = {CssRule.position: 'rel'};
 
 // zIndex
-final zIndex = ({dynamic index}) => {CssRule.zIndex: index};
+final FL_ZIndex = ({dynamic index}) => {CssRule.zIndex: index};
 
 // scroll
-final overScrollY = {CssRule.overflowY: 'scroll'};
-final overScrollX = {CssRule.overflowX: 'scroll'};
+final FL_OverScrollY = {CssRule.overflowY: 'scroll'};
+final FL_OverScrollX = {CssRule.overflowX: 'scroll'};
 
 // flex
-final flex = ({dynamic size}) => {CssRule.flex: size};
+final FL_Flex = ({dynamic size}) => {CssRule.flex: size};
 
 // size
-final minWidth = ({dynamic size}) => {CssRule.minWidth: size};
-final minHeight = ({dynamic size}) => {CssRule.minHeight: size};
-final maxWidth = ({dynamic size}) => {CssRule.maxWidth: size};
-final maxHeight = ({dynamic size}) => {CssRule.maxHeight: size};
+final FL_MinWidth = ({dynamic size}) => {CssRule.minWidth: size};
+final FL_MinHeight = ({dynamic size}) => {CssRule.minHeight: size};
+final FL_MaxWidth = ({dynamic size}) => {CssRule.maxWidth: size};
+final FL_MaxHeight = ({dynamic size}) => {CssRule.maxHeight: size};
 
 // abs
-final absTop = ({dynamic size}) => {CssRule.top: size};
-final absLeft = ({dynamic size}) => {CssRule.left: size};
-final absRight = ({dynamic size}) => {CssRule.right: size};
-final absBottom = ({dynamic size}) => {CssRule.bottom: size};
+final FL_AbsTop = ({dynamic size}) => {CssRule.top: size};
+final FL_AbsLeft = ({dynamic size}) => {CssRule.left: size};
+final FL_AbsRight = ({dynamic size}) => {CssRule.right: size};
+final FL_AbsBottom = ({dynamic size}) => {CssRule.bottom: size};
 
 // opacity
-final opacity = ({dynamic size}) => {CssRule.opacity: size};
+final FL_Opacity = ({dynamic size}) => {CssRule.opacity: size};
 
 // backgroundSize
-final bgSize = ({dynamic size}) => {CssRule.backgroundSize: size};
+final FL_BgSize = ({dynamic size}) => {CssRule.backgroundSize: size};
 
 // scrollBar
-final scrollBar = ({dynamic state}) => {CssRule.scrollBar: state};
+final FL_ScrollBar = ({dynamic state}) => {CssRule.scrollBar: state};
 
 // font
-final fWeight = ({dynamic size}) => {CssRule.fontWeight: size};
-final fFamily = ({dynamic size}) => {CssRule.fontFamily: size};
+final FL_FWeight = ({dynamic size}) => {CssRule.fontWeight: size};
+final FL_FFamily = ({dynamic size}) => {CssRule.fontFamily: size};
 
 // backgroundImage
-final bgImage = ({dynamic size}) => {CssRule.backgroundImage: size};
+final FL_BgImage = ({dynamic size}) => {CssRule.backgroundImage: size};
 
 // backgroungSize
-final bgSizeCover = {CssRule.backgroundSize: 'cover'};
-final bgSizeContain = {CssRule.backgroundSize: 'contain'};
-final bgSizeFill = {CssRule.backgroundSize: 'fill'};
-final bgSizeFitHeight = {CssRule.backgroundSize: 'fitHeight'};
-final bgSizeFitWidth = {CssRule.backgroundSize: 'fitWidth'};
-final bgSizeScaleDown = {CssRule.backgroundSize: 'scaleDown'};
+final FL_BgSizeCover = {CssRule.backgroundSize: 'cover'};
+final FL_BgSizeContain = {CssRule.backgroundSize: 'contain'};
+final FL_BgSizeFill = {CssRule.backgroundSize: 'fill'};
+final FL_BgSizeFitHeight = {CssRule.backgroundSize: 'fitHeight'};
+final FL_BgSizeFitWidth = {CssRule.backgroundSize: 'fitWidth'};
+final FL_BgSizeScaleDown = {CssRule.backgroundSize: 'scaleDown'};
 
 // transform
-final transform = ({dynamic matrix4}) => {CssRule.transform: matrix4};
+final FL_Transform = ({dynamic matrix4}) => {CssRule.transform: matrix4};
